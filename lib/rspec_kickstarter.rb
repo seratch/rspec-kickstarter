f0e8012c732571d1bc1f322048c8d8450c0e8056
Kazuhiro Sera <seratch@gmail.com>
2013-05-01 19:28:56 +0900

    Added default value

--- lib/rspec_kickstarter.rb
+++ lib/rspec_kickstarter.rb
@@ -5,7 +5,7 @@ require "rspec_kickstarter/version"
 
 module RSpecKickstarter
 
-  def self.write_spec(file_path, spec_dir, force_write = false, dry_run = false)
+  def self.write_spec(file_path, spec_dir = './spec', force_write = false, dry_run = false)
     generator = RSpecKickstarter::Generator.new(spec_dir)
     generator.write_spec(file_path, force_write, dry_run)
   end
